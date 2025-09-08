c330d5d9068c6f371610f33866c81a84db2722c1
mohamed aziz dhaya <145987753+medazizdhaya26@users.noreply.github.com>
2025-09-08 14:30:30 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), sentryVitePlugin({
-    org: "jsm-x9",
-    project: "javascript-react"
+  base: '/iphone15-official-clone/'
   })],
 
   build: {
