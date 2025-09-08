3a878e98a5187f317044d2906ad7248711e79d6f
mohamed aziz dhaya <azizdhaya26@gmail.com>
2025-09-08 15:53:27 +0100

    all the projects

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/iphone15-official-clone/',
-
-  plugins: [react(), sentryVitePlugin({
-  })],
-
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/iphone15-official-clone/" : "/", // ✅ dynamic
+  plugins: [
+    react(),
+    sentryVitePlugin({})
+  ],
   build: {
     sourcemap: true
   }
-})
+}));
